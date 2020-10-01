1313d9e59c21417cf8a148b761a723e783c04dff
Laura Gomez <gomez.lcgl@gmail.com>
2020-10-01 11:03:32 -0700

    update matlab functions

--- matlab/plotting.m
+++ matlab/plotting.m
@@ -1,15 +1,17 @@
-% load data
-addpath('matlab','data')
-load('calcium_data.mat')
-load('stimuli_info.mat')
-
 % data summary
 num_fields = 8;
 dat_summary = get_info(data,stimuli_info, num_fields);
 
+figure (1)
+plot(data{1}.ROI1.movie_1.dff)
+
+figure(2)
 plot(data{1}.ROI1.movie_1.deNoise_dff)
 hold on 
-% plot(data{1}.ROI1.movie_1.dff)
-
 dff_deriv = diff(data{1}.ROI1.movie_1.deNoise_dff);
 plot(dff_deriv)
+
+figure(3)
+plot(data{1}.ROI1.movie_1.spike)
+
+std_roi3 = std(data{1}.ROI3.movie_1.dff)
